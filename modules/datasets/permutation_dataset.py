c8b9fce574940b5d1fd6cd5efae6e2d96139dc79
Tjark Prokoph <tjark.prokoph@gmx.de>
2023-03-04 19:22:44 +0100

    Changed usage of the PermutationDataset Class

--- modules/datasets/permutation_dataset.py
+++ modules/datasets/permutation_dataset.py
@@ -1,62 +1,47 @@
 import tensorflow as tf
+import itertools as it
 
 from collections.abc import Callable
 
 
-class PermutationDataset(tf.data.Dataset):
+class PermutationDataset():
     """
     Dataset which contains the logic for initializing
     permutation of all input textual sequences in the
-    base dataset.
-
-    Attributes
-    ----------
-    separate: Callable[[str], list[str]]
-        Separation function for textual sequences.
-    convert: Callable[[str], list]
-        Conversion function for textual sequences into vector representation.
+    base text dataset.
     
     Methods
     -------
-    from_dataset(base_dataset: tf.data.Dataset)
-        Creates a permutation tf.data.Dataset from the base dataset 
+    from_dataset(base_text_dataset: tf.data.Dataset, separate: Callable[[str], list[str]], convert: Callable[[str], list])
+        Creates a permutation tf.data.Dataset from the base text dataset 
         based on the provided separation and conversion functions.
     """
 
-    __seperate: Callable[[str], list[str]] = None
-    __convert: Callable[[str], list] = None
-
-    def __init__(
-            self, 
-            separate: Callable[[str], list[str]], 
+    @staticmethod
+    def from_dataset(
+            base_text_dataset: tf.data.Dataset,
+            separate: Callable[[str], list[str]],
             convert: Callable[[str], list]
-            ) -> None:
+        ) -> tf.data.Dataset:
         """
-        Dataset which contains the logic for initializing
-        permutation of all input textual sequences in the
-        base dataset.
+        Creates a permutation tf.data.Dataset from the base dataset 
+        based on the provided separation and conversion functions.
 
         Parameters
         ----------
+        base_text_dataset: tf.data.Dataset
+            Base text dataset which needs to be explained.
         separate: Callable[[str], list[str]]
             Separation function for textual sequences.
         convert: Callable[[str], list]
             Conversion function for textual sequences into vector representation.
         """
-
-        self.__seperate = separate
-        self.__convert = convert
-
-    def from_dataset(self, base_dataset: tf.data.Dataset) -> None:
-        """
-        Creates a permutation tf.data.Dataset from the base dataset 
-        based on the provided separation and conversion functions.
-
-        Parameters
-        ----------
-        base_dataset: tf.data.Dataset
-            Base dataset which needs to be explained.
-        """
         
-        seperated_dataset = base_dataset.map(self.__seperate, num_parallel_calls=tf.data.AUTOTUNE)
-        converted_dataset = base_dataset.map(self.__convert, num_parallel_calls=tf.data.AUTOTUNE)
+        separated_dataset = base_text_dataset.map(separate, num_parallel_calls=tf.data.AUTOTUNE)
+        converted_dataset = base_text_dataset.map(convert, num_parallel_calls=tf.data.AUTOTUNE)
+        permutation_dataset = tf.data.Dataset.zip((separated_dataset, converted_dataset))
+
+        def __permute_entry__(separated_entry: tf.Tensor, converted_entry: tf.Tensor):
+            return separated_entry, converted_entry, it.product(range(2), repeat=len(separated_entry))
+
+        return permutation_dataset.map(__permute_entry__, num_parallel_calls=tf.data.AUTOTUNE)
--- modules/explaination/effect_explainer.py
+++ modules/explaination/effect_explainer.py
@@ -5,8 +5,8 @@ from modules.explaination.effect_explaination import EffectExplaination
 
 
 class EffectExplainer():
-    __meta_dataset: BoWCosineDataset = None
-    __significant_effect_dataset: tf.data.Dataset = None
+    __meta_dataset: BoWCosineDataset
+    __significant_effect_dataset: tf.data.Dataset
 
     def __init__(self, significant_effect_dataset: tf.data.Dataset) -> None:
         self.__significant_effect_dataset = significant_effect_dataset
