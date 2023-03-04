be770a5cc6d8ec58f1b8bef07bb706a4dda6a2e7
Tjark Prokoph <tjark.prokoph@gmx.de>
2023-03-04 18:22:46 +0100

    Added initialization of PermutationDataset

--- modules/decomposition/model_decomposer.py
+++ modules/decomposition/model_decomposer.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+from collections.abc import Callable
 from modules.datasets.effect_dataset import EffectDataset
 from modules.datasets.permutation_dataset import PermutationDataset
 from modules.decomposition.model_decomposition import ModelDecomposition
@@ -14,10 +15,13 @@ class ModelDecomposer():
     def __init__(
             self, 
             base_dataset: tf.data.Dataset, 
-            base_model: tf.keras.Model
+            base_model: tf.keras.Model,
+            separate: Callable[[str], list[str]], 
+            convert: Callable[[str], list]
             ) -> None:
         self.__base_dataset = base_dataset
         self.__base_model = base_model
+        self.__permutation_dataset = PermutationDataset(separate, convert)
     
     def decompose(self) -> ModelDecomposition:
         return ModelDecomposition(self.__permutation_dataset, self.__effect_dataset)
--- modules/tcm_de.py
+++ modules/tcm_de.py
@@ -1,3 +1,4 @@
+from collections.abc import Callable
 from modules.explaination.effect_explainer import EffectExplainer
 from modules.decomposition.model_decomposer import ModelDecomposer
 
@@ -5,6 +6,13 @@ from modules.decomposition.model_decomposer import ModelDecomposer
 class TCMDE():
     __effect_explainer: EffectExplainer = None
     __model_decomposer: ModelDecomposer = None
+    __seperate: Callable[[str], list[str]] = None
+    __convert: Callable[[str], list] = None
 
-    def __init__(self) -> None:
-        pass
+    def __init__(
+            self,
+            separate: Callable[[str], list[str]], 
+            convert: Callable[[str], list]
+            ) -> None:
+        self.__seperate = separate
+        self.__convert = convert
